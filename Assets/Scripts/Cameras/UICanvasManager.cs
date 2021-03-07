c3d7403a218f427828215e1f1a98ad8ff1ae4417
NickFrangie <51765298+NickFrangie@users.noreply.github.com>
2021-03-07 13:28:34 -0500

    ScoreUI
    Added ScoreUI Implementation and the FontUtils ability to get a Font from any digit in a number.

--- Assets/Scripts/Cameras/UICanvasManager.cs
+++ Assets/Scripts/Cameras/UICanvasManager.cs
@@ -13,11 +13,14 @@ public class UICanvasManager : MonoBehaviour
     static private float[] SCALAR = new float[3] { 1, .75f, .5f };
     static private float HEALTH_FADE_TIME = .5f;
     static private float HEALTH_FADE_ALPHA = .4f;
+    static private int SCORE_RAISE_SPEED = 5000;
     
     // Variables
     private int playerValue;
     private int healthDisplayed = 0;
-    private int healthTarget; // 0-3 for number of health
+    private int healthTarget = 0; // 0-3 for number of health
+    private int scoreDisplayed = 0;
+    private int scoreTarget = 0;
 
     // Components
     private RectTransform healthUI;
@@ -52,8 +55,31 @@ public class UICanvasManager : MonoBehaviour
     }
 
     #region Update UI Elements
-        public void UpdateScore(int newTotal, int addedScore) {
+        public void UpdateScore(int addedScore) {
+            scoreTarget += addedScore;
+            StartCoroutine(ScoreLerp());
+        }
 
+        private IEnumerator ScoreLerp() {
+            int scoreStart = scoreDisplayed;
+            float lerpT = 0;
+            float fadeTime = (float) (scoreTarget - scoreStart) / (float) SCORE_RAISE_SPEED;
+
+            while (lerpT < 1) {
+                lerpT += Time.deltaTime / fadeTime;
+                lerpT = Mathf.Clamp(lerpT, 0, 1);
+                scoreDisplayed = (int) Mathf.Lerp(scoreStart, scoreTarget, lerpT);
+                SetScoreUI(scoreDisplayed);
+                yield return new WaitForEndOfFrame();
+            }
+            yield return null;
+        }
+
+        private void SetScoreUI(int score) {
+            SpriteRenderer[] spriteRenderers = scoreUI.GetComponentsInChildren<SpriteRenderer>();
+            for (int i = spriteRenderers.Length - 1; i >= 1; i--) {
+                spriteRenderers[i].sprite = FontUtils.DigitInNumToFont(score, spriteRenderers.Length - 1 - i);
+            } 
         }
 
         public void UpdateStreak(int streak) {
